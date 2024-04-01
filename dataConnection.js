6e32cc307f7172b136c0f3c40fa76402d25d7f57
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-01 00:50:15 -0500

    update dataConnection.js

--- dataConnection.js
+++ dataConnection.js
@@ -1,6 +1,6 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://imalgoubri2:<Ilovedatabases#>@cluster0.et25buk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://imalgoubri2@huskers.unl.edu:<Ilovedatabases#>@cluster0.et25buk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
