f8a56449493f38f564e3a1525adcf32c5e35beaf
Inoussa7 <inoussa.malgoubri@fulbrightmail.org>
2024-04-01 00:27:47 -0500

    update dataConnection.js

--- dataConnection.js
+++ dataConnection.js
@@ -1,5 +1,5 @@
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = "mongodb+srv://imalgoubri2:<Ilovedatabases#>@cluster0.et25buk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -11,16 +11,4 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+module.exports = client;
